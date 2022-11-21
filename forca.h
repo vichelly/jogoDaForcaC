419b47fbc2702fbc8d9b9778987be0cb79ea1e6c
Vitor <vlucasff@hotmail.com>
2022-11-20 23:59:12 -0300

    first commit

---
+++ forca.h
@@ -0,0 +1,12 @@
+//declarar funções para não ficar declarando em ordem
+void abertura();
+void chuta();
+void desenhaforca();
+void escolhepalavra();
+int acertou();
+int enforcou();
+int jachutou(char letra);
+void adicionapalavra();
+void desenhaforca();
+int letraexiste(char letra);
+int chuteserrados();
